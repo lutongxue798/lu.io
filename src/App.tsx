5b3f6257724abf4a9dcef302b9aef51ebdad7d05
luwenrui <1365240681@qq.com>
2024-04-07 20:20:23 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import MyHeader from './components/header'
+import Project from './components/project'
+
+function App() {
+  return (
+      <div class='warp'>
+        <MyHeader></MyHeader>
+        <Project></Project>
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/project.tsx
@@ -0,0 +1,13 @@
+export default function Project() {
+    return (
+        <article class="mt-10">
+            <h1 class=" text-red mb5">Project</h1>
+            <section>
+                <div>
+                    <img src="" class="w-30 h-30 block"/>
+                    <span>test</span>
+                </div>
+            </section>
+        </article>
+    )
+}
