1e29d803700936fc9d4193713130a7712e18dbeb
luwenrui <1365240681@qq.com>
2024-06-04 23:29:12 +0800

    chore: My Profile

---
+++ uno.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig,presetWebFonts} from 'unocss'
+
+export default defineConfig({
+    shortcuts:{
+        'box':'border rd-4px border-gray-7 text-white bg-#212529 py4 rd-4',
+        'line':'border-b border-gray-7',
+    },
+})
