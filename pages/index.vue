63dda5e2df615a3ca7aab71d88d82f3a2b0e3951
luwenrui <1365240681@qq.com>
2024-06-05 20:15:16 +0800

    chore: init

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <Logos mb-6 />
+    <Suspense>
+      <ClientOnly>
+
+      </ClientOnly>
+      <template #fallback>
+        <div italic op50>
+          <span animate-pulse>Loading...</span>
+        </div>
+      </template>
+    </Suspense>
+    <InputEntry />
+  </div>
+</template>
